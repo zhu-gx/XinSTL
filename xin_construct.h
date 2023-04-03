4e20f953fe3738e3f4d12294665dfbd44683a6cf
zhu-gx <1031464680@qq.com>
2023-04-03 20:50:36 +0800

    完成construct，包括了对于普通类和迭代器的构造和析构，其中析构函数通过type_traits的不同，采取不同的模板偏特化进行析构

--- xin_construct.h
+++ xin_construct.h
@@ -4,6 +4,7 @@
 #include<new> //placement new
 #include<xin_type_traits.h>
 #include<xin_iterator.h>
+#include<xin_utility.h>
 
 namespace XinSTL{
     //construct构造对象
@@ -17,6 +18,44 @@ namespace XinSTL{
         ::new ((void*)ptr) T1(value);
     }
 
-    template
+    template<class T,class... Args>
+    void construct(T* ptr,Args&&... args){
+        ::new ((void*)ptr) T(XinSTL::forward<Args>(args)...);
+    }
+
+    //destroy将对象析构
+    //以下是类类型的析构，根据type_traits采取不同的析构偏特化
+    //普通类型，无需采用多余的析构函数
+    template<class T>
+    void destroy_one(T*,std::true_type){}
+
+    //需要调用指定的析构函数
+    template<class T>
+    void destroy_one(T* pointer,std::false_type){
+        if(pointer != nullptr){
+            pointer->~T();
+        }
+    }
+
+    template<class T>
+    void destroy(T* pointer){
+        destroy_one(pointer,std::is_trivially_destructible<T>{});
+    }
+
+    //以下是迭代器的析构，也是根据type_traits的不同采取不同的析构策略
+    template<class ForwardIterator>
+    void destroy_category(ForwardIterator,ForwardIterator,std::true_type){}
+
+    template<class ForwardIterator>
+    void destroy_category(ForwardIterator first,ForwardIterator last,std::false_type){
+        for(;first != last;first++){
+            destroy(&*first);
+        }
+    }
+
+    template<class ForwardIterator>
+    void destroy(ForwardIterator first,ForwardIterator last){
+        destroy_category(first,last,std::is_trivially_destructible<ForwardIterator>::value_type{});
+    }
 
 }

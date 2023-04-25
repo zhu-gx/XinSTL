97ed698039a62c5b291cc5830ef671c76d1ce01e
zhu-gx <1031464680@qq.com>
2023-04-26 00:08:46 +0800

    vector

--- xin_vector.h
+++ xin_vector.h
@@ -563,4 +563,246 @@ namespace XinSTL{
         init_space(len,init_size);
         XinSTL::uninitialized_copy(first,last,begin_);
     }
+
+    //destroy_and_recover函数
+    template<class T>
+    void vector<T>::destroy_and_recover(iterator first,iterator last,size_type n){
+        data_allocator::destroy(first,last);
+        data_allocator::deallocate(first,n);
+    }
+
+    //get_new_cap函数
+    template<class T>
+    typename vector<T>::size_type
+    vector<T>::get_new_cap(size_type add_size){
+        const auto old_size = capacity();
+        THROW_LENGTH_ERROR_IF(old_size > max_size() - add_size,"vector<T>'s size too big");
+        if(old_size > max_size()-old_size/2){
+            return old_size + add_size > max_size()-16 ? old_size + add_size : old_size + add_size + 16;
+        }
+        const size_type new_size = old_size == 0 ? XinSTL::max(add_size,static_cast<size_type>(16)) : XinSTL::max(old_size + old_size/2,old_size + add_size);
+        return new_size;
+    }
+
+    //fill_assign函数
+    template<class T>
+    void vector<T>::fill_assign(size_type n,const value_type& value){
+        if(n > capacity()){
+            vector temp(n,value);
+            swap(temp);
+        }else if(n > size()){
+            XinSTL::fill(begin(),end(),value);
+            end_ = XinSTL::uninitialized_fill_n(end_,n-size(),value);
+        }else{
+            erase(XinSTL::fill_n(begin_,n,value),end_);
+        }
+    }
+
+    //copy_assign函数
+    template<class T,class Iterator>
+    void vector<T>::copy_assign(Iterator first,Iterator last,input_iterator_tag){
+        auto cur = begin_;
+        for(;first != last && cur != end_;first++,cur++){
+            *cur = *first;
+        }
+        if(first == last){
+            erase(cur,end_);
+        }else{
+            insert(end_,first,last);
+        }
+    }
+
+    //用[first,last)为容器赋值
+    template<class T,class ForwardIterator>
+    void vector<T>::copy_assign(ForwardIterator first,ForwardIterator last,forward_iterator_tag){
+        const size_type len = XinSTL::distance(first,last);
+        if(len > capacity()){
+            vector temp(first,last);
+            swap(temp);
+        }else if(size() >= len){
+            auto new_end = XinSTL::copy(first,last,begin_);
+            data_allocator::destroy(new_end,end_);
+            end_ = new_end;
+        }else{
+            auto mid = first;
+            XinSTL::advance(mid,size());
+            XinSTL::copy(first,mid,begin_);
+            auto new_end = XinSTL::uninitialized_copy(mid,last,end_);
+            end_ = new_end;
+        }
+    }
+
+    //重新分配空间并在pos处就地构造元素
+    template<class T,class ...Args>
+    void vector<T>::reallocate_emplace(iterator pos,Args&& ..args){
+        const auto new_size = get_new_cap(1);
+        auto new_begin = data_allocator::allocate(new_size);
+        auto new_end = new_begin;
+        try{
+            new_end = XinSTL::uninitialized_move(begin_,pos,new_begin);
+            data_allocator::construct(XinSTL::address_of(*new_end),XinSTL::forward<Args>(args)...);
+            new_end++;
+            new_end = XinSTL::uninitialized_move(pos,end_,new_end);
+        }catch{
+            data_allocator::deallocate(new_begin,new_size);
+            throw;
+        }
+        destroy_and_recover(begin_,end_,cap_ - begin_);
+        begin_ = new_begin;
+        end_ = new_end;
+        cap_ = new_begin + new_size;
+    }
+
+    //fill_insert函数
+    template<class T>
+    typename vector<T>::iterator
+    vector<T>::fill_insert(iterator pos,size_type n,const value_type& value){
+        if(n == 0){
+            return pos;
+        }
+        const size_type xpos = pos - begin_;
+        const value_type value_copy = value;//避免被覆盖
+        if(static_cast<size_type>(cap_ - end_) >= n){
+            //如果备用空间 >= 增加的空间
+            const size_type after_elems = end_ - pos;
+            auto old_end = end_;
+            if(after_elems > n){
+                XinSTL::uninitialized_copy(end_ - n,end_,end_);
+                end_ += n;
+                XinSTL::move_backward(pos,old_end - n,old_end);
+                XinSTL::uninitialized_fill_n(pos,n,value_copy);
+            }else{
+                end_ = XinSTL::uninitialized_fill_n(end_,n - after_elems,value_copy);
+                end_ = XinSTL::uninitialized_move(pos,old_end,end_);
+                XinSTL::uninitialized_fill_n(pos,after_elems,value_copy);
+            }
+        }else{
+            //如果备用空间不足
+            const auto new_size = get_new_cap(n);
+            auto new_begin = data_allocator::allocate(new_size);
+            auto new_end = new_begin;
+            try{
+                new_end = XinSTL::uninitialized_move(begin_,pos,new_begin);
+                new_end = XinSTL::uninitialized_fill_n(new_end,n,value);
+                new_end = XinSTL::uninitialized_move(pos,end_,new_end);
+            }catch{
+                destroy_and_recover(new_begin,new_end,new_size);
+                throw;
+            }
+            data_allocator::deallocate(begin_,cap_ - begin_);
+            begin_ = new_begin;
+            end_ = new_end;
+            cap_ = begin_ + new_size;
+        }
+        return begin_ + xpos;
+    }
+
+    //copy_insert函数
+    template<class T,class InputIterator>
+    void vector<T>::copy_insert(iterator pos,InputIterator first,InputIterator last){
+        if(first == last){
+            return;
+        }
+        const auto n = XinSTL::distance(first,last);
+        if((cap_ - end_) >= n){
+            //如果备用空间大小足够
+            const auto after_elems = end_ - pos;
+            auto old_end = end_;
+            if(after_elems > n){
+                end_ = XinSTL::uninitialized_copy(end_ - n,end_,end_);
+                XinSTL::move_backward(pos,old_end - n,old_end);
+                XinSTL::uninitialized_copy(first,last,pos);
+            }else{
+                auto mid = first;
+                XinSTL::advance(mid,after_elems);
+                end_ = XinSTL::uninitialized_copy(mid,last,end_);
+                end_ = XinSTL::uninitialized_move(pos, old_end, end_);
+                XinSTL::uninitialized_copy(first, mid, pos);
+            }
+        }else{
+            //备用空间不足
+            const auto new_size = get_new_cap(n);
+            auto new_begin = data_allocator::allocate(new_size);
+            auto new_end = new_begin;
+            try{
+                new_end = XinSTL::uninitialized_move(begin_,pos,new_begin);
+                new_end = XinSTL::uninitialized_copy(first,last,new_end);
+                new_end = XinSTL::uninitialized_move(pos,end_,new_end);
+            }catch(...){
+                destroy_and_recover(new_begin,new_end,new_size);
+                throw;
+            }
+            data_allocator::deallocate(begin_,cap_ - begin_);
+            begin_ = new_begin;
+            end_ = new_end;
+            cap_ = begin_ + new_size;
+        }
+    }
+
+    // reinsert 函数
+    template <class T>
+    void vector<T>::reinsert(size_type size)
+    {
+        auto new_begin = data_allocator::allocate(size);
+        try
+        {
+            XinSTL::uninitialized_move(begin_, end_, new_begin);
+        }
+        catch (...)
+        {
+            data_allocator::deallocate(new_begin, size);
+            throw;
+        }
+        data_allocator::deallocate(begin_, cap_ - begin_);
+        begin_ = new_begin;
+        end_ = begin_ + size;
+        cap_ = begin_ + size;
+    }
+
+    /*****************************************************************************************/
+    // 重载比较操作符
+
+    template <class T>
+    bool operator==(const vector<T>& lhs, const vector<T>& rhs)
+    {
+        return lhs.size() == rhs.size() &&
+        XinSTL::equal(lhs.begin(), lhs.end(), rhs.begin());
+    }
+
+    template <class T>
+    bool operator<(const vector<T>& lhs, const vector<T>& rhs)
+    {
+        return XinSTL::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+
+    template <class T>
+    bool operator!=(const vector<T>& lhs, const vector<T>& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    template <class T>
+    bool operator>(const vector<T>& lhs, const vector<T>& rhs)
+    {
+        return rhs < lhs;
+    }
+
+    template <class T>
+    bool operator<=(const vector<T>& lhs, const vector<T>& rhs)
+    {
+        return !(rhs < lhs);
+    }
+
+    template <class T>
+    bool operator>=(const vector<T>& lhs, const vector<T>& rhs)
+    {
+        return !(lhs < rhs);
+    }    
+
+    // 重载 mystl 的 swap
+    template <class T>
+    void swap(vector<T>& lhs, vector<T>& rhs)
+    {
+        lhs.swap(rhs);
+    }
 }
